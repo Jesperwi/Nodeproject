e91989fae480d57bd2831e5e8244b3bd68f8956c
Jesperwi <71511557+Jesperwi@users.noreply.github.com>
2020-10-30 17:26:51 +0100

    third commit

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-ECHO is on.
+const express = require('express');
+      morgan = require('morgan');
+
+const app = express();
+
+app.use('/documentation', express.static('documentation'));
+
+app.use(morgan('common'));
+
+let topMovies = [
+    {
+        title: 'Harry Pantter and the Sorceres\'s Stone',
+        author: 'J.K. Rowling'
+    },
+    {
+        title: 'Lord of the Rings',
+        author: 'J.R.R. Tolkien'
+    },
+    {
+        title: 'Twilight',
+        author: 'Stephanie Meyer'
+    }
+];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie app!');
+});
+  
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+  
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
