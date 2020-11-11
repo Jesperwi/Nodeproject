d17b0f6f8e38da4e29bab85c5477481017adcbbf
Jesperwi <71511557+Jesperwi@users.noreply.github.com>
2020-11-11 14:03:41 +0100

    task2.10

--- index.js
+++ index.js
@@ -225,12 +225,8 @@ app.use(cors({
 }));
 
 
-
-
-
-
-
-
+mongoose.connect( process.env.CONNECTION_URI,
+{ useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.use((err, req, res, next) => {
@@ -243,6 +239,8 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
   console.log('Listening on Port' + port);
 });
+
+
 
 
 
