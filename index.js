c891f523d9ab43657f2303031a01ac4ef9e2f43b
Jesperwi <71511557+Jesperwi@users.noreply.github.com>
2020-11-11 16:02:53 +0100

    task 2.10

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const { check, validationResult } = require('express-validator');
 
 const Movies = Models.Movie;
 const Users = Models.User;     
-      
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-  
+
+mongoose.connect( process.env.CONNECTION_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
 morgan = require('morgan');
   
 app = express();
@@ -24,11 +25,25 @@ require('./passport')
 
 uuid = require('uuid');
 
-app.use('/public', express.static('documentation'));
+app.use(express.static('public'));
 
 app.use(morgan('common'));
 
 app.use(cors());
+
+//cors usage
+let allowedOrigins = ['http://localhost:8080', 'https://jwmyflix.herokuapp.com/'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ 
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // GET requests
 app.get('/', (req, res) => { 
@@ -209,24 +224,6 @@ app.delete('/users/:Username', (req, res) => {
     });
 });
 
-
-//cors usage
-let allowedOrigins = ['http://localhost:8080', 'https://jwmyflix.herokuapp.com/'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ 
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
-
-
-mongoose.connect( process.env.CONNECTION_URI,
-{ useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.use((err, req, res, next) => {
