4b16d7cccf56bcecd118f20eb0deddd78ec66a5c
Jesperwi <71511557+Jesperwi@users.noreply.github.com>
2020-11-11 15:32:29 +0100

    task 2.10

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to my myflix!');
 });
 
-app.get('/movies', passport.authenticate('jwt', { session: false}),(req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
   .then((movies) => {
     res.status(201).json(movies);
